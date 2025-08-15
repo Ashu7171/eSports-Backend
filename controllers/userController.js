727bc44c51d8e3f9215c2fe76fe9513e04fdd5da
Ashutosh Gurav <ashutoshgurav71@gmail.com>
2025-08-15 17:20:26 +0530

    functions added to controller and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
 
 // Create User
 const createUser = async (req, res) => {
@@ -72,4 +74,80 @@ const deleteUser = async (req, res) => {
     }
 };
 
-module.exports = { createUser, getUsers, getUserById, updateUser, deleteUser };
+const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Generate reset token valid for 15 minutes
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '15m' });
+
+    // Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+    });
+
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password/${token}`;
+
+    await transporter.sendMail({
+      to: email, // Nodemailer uses EMAIL_USER as sender automatically
+      subject: "Password Reset Link",
+      html: `<p>Click <a href="${resetLink}">here</a> to reset your password. This link is valid for 15 minutes.</p>`
+    });
+
+    res.json({ message: "Reset link sent to email" });
+
+  } catch (err) {
+    console.log("Email error:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// RESET PASSWORD
+const resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Hash new password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    res.json({ message: "Password reset successful" });
+
+  } catch (err) {
+    console.log("Reset error:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if(!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    res.json({ token, message: "Login successful" });
+
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+module.exports = { createUser, getUsers, getUserById, updateUser, deleteUser, forgotPassword, resetPassword ,loginUser};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { createUser, getUsers, getUserById, updateUser, deleteUser } = require('../controllers/userController');
+
+const { createUser, getUsers, getUserById, updateUser, deleteUser ,forgotPassword, resetPassword ,loginUser } = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -8,5 +9,13 @@ router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+router.post('/login', loginUser);
+
+// POST request to send reset link
+router.post('/forgot-password', forgotPassword);
+
+// POST request to reset password using token
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
